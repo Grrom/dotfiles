e98cef1622960b8a0f5181e1d9f1bc1ff62c1433
Jerome <josejeromelalunio@gmail.com>
2021-08-15 07:06:49 +0800

    add my personal script

---
+++ code/personal/scripts/personal.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# aliases
+alias bashrc="nvim ~/.bashrc"
+alias nvimrc="nvim ~/.config/nvim/init.vim"
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+
+# paths
+export PATH="$PATH:~/code/dart/channels/stable/bin"
+
+
+# commands
+
+# android emulators 
+emu(){
+  ACTION=$1
+  OPEN=$2
+
+  case "$OPEN" in 
+    "sm") OPEN="Pixel_3a_API_24";;
+  esac
+  
+  case "$ACTION" in
+    "l") ~/Android/Sdk/tools/emulator -list-avds;;
+    "o") echo "Opening $OPEN" ; ~/Android/Sdk/tools/emulator @$OPEN;;
+  esac
+}
