216d4b3d50e69cc13120d3ddeb6db970ca59368e
Jerome <josejeromelalunio@gmail.com>
2021-12-13 16:17:41 +0800

    added stuff

--- code/personal/scripts/project_shortcuts.sh
+++ code/personal/scripts/project_shortcuts.sh
@@ -2,4 +2,7 @@ alias memorylamp="code ~/code/dart/memorylamp-mobile"
 alias dating="code ~/code/dart/boiling_waters_dating"
 alias church="code ~/code/javascript/church_information_system"
 alias lofi="code ~/code/typescript/lofi-moods"
+alias music="code ~/code/typescript/music"
+
+alias riceNotes="nvim ~/code/personal/personalStuff/rice-notes.json"
 
